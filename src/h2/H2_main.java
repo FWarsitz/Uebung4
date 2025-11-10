052de19dd55a9c48c8ad20b9d881c77734ca7422
FWarsitz <fwarsitz@smail.uni-koeln.de>
2025-11-10 14:29:36 +0100

    Hausaufgabe2

---
+++ src/h2/H2_main.java
@@ -0,0 +1,59 @@
+package h2;
+
+public class H2_main {
+
+	public static void main(String[] args) {
+		boolean x,y = false;
+		boolean a,b,c = false;
+		int input =  0;
+		
+		switch (input) {
+		case 0:
+			x = false;
+			y = false;
+			break;
+		case 1:
+			x = false;
+			y = true;
+			break;
+		case 10:
+			x = true;
+			y = false;
+			break;
+		case 11:
+			x = true;
+			y = false;
+			break;
+		default:
+			x = false;
+			y = false;
+			break;
+		}
+		
+		//a
+		if (x == true && y == true) {
+			a = true;
+		}
+		else {
+			a = false;
+		}
+		
+		//b
+		if (x == true || x != y){
+			b = true;
+		}
+		else {
+			b = false;
+		}
+		
+		//c
+		if ((x == true && y == false) || (x == false && y == false)) {
+			c = true;
+		}
+		else {
+			c = false;
+		}
+		
+	}
+
+}
